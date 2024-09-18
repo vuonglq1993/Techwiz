4f3fe868e36dfa57021025ee953d89e71c6c72fa
ThaiChuT9 <iamtk4i@gmail.com>
2024-09-18 10:20:02 +0700

    test

---
+++ testt/src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Header from "./components/Common/Header/Header";
+import Footer from "./components/Common/Footer/Footer";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
