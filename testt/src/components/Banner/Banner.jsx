f76276c36c59f04ae9546fdd38095008634a3157
ThaiChuT9 <iamtk4i@gmail.com>
2024-09-19 21:07:17 +0700

    fix

--- testt/src/components/Banner/Banner.jsx
+++ testt/src/components/Banner/Banner.jsx
@@ -1,53 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-import { Carousel } from "react-bootstrap";
-import sliderImg from "../../assets/images/slider/1.png";
-import sliderImg1 from "../../assets/images/slider/2.png";
-import "../Banner/banner.css"
-=======
 import "../Banner/banner.css";
 import bannervideo from "../../assets/video/bannervideo.mp4";
->>>>>>> origin/main
 
 const Banner = () => {
   return (
     <>
       <section className="slider">
-<<<<<<< HEAD
-        <Carousel variant="dark">
-          <Carousel.Item>
-            <img src={sliderImg} className="d-block w-100" alt="First slide" />
-            <Carousel.Caption>
-              <div className="slider_des">
-                <h5 className="heading">
-                  JOURNEY TO <span>EXPLORE WORLD</span>
-                </h5>
-                <p className="sub_text">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </p>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img src={sliderImg1} className="d-block w-100" alt="First slide" />
-            <Carousel.Caption>
-              <div className="slider_des">
-                <h5 className="heading">
-                  BEAUTIFUL PLACE <span>TO VISIT</span>
-                </h5>
-                <p className="sub_text">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </p>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-=======
         <div className="video-wrapper">
         <video
             width="100%"
@@ -70,7 +28,6 @@ const Banner = () => {
             {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. */}
           </p>
         </div>
->>>>>>> origin/main
       </section>
     </>
   );
--- testt/src/components/Common/Header/Header.jsx
+++ testt/src/components/Common/Header/Header.jsx
@@ -8,19 +8,12 @@ import {
 } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import BookingModal from "../../Booking Modal/BookingModal";
-<<<<<<< HEAD
-=======
 // import logo from "../../assets/images/logo.png"; // Đường dẫn logo của bạn
->>>>>>> origin/main
 import "../Header/header.css";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
-<<<<<<< HEAD
-  const [isModalOpen, setModalOpen] = useState(false); // Trạng thái cho modal
-=======
   const [isModalOpen, setModalOpen] = useState(false);
->>>>>>> origin/main
 
   const toggleMenu = () => {
     setOpen(!open);
@@ -30,23 +23,6 @@ const Header = () => {
     setModalOpen(!isModalOpen);
   };
 
-<<<<<<< HEAD
-  useEffect(() => {
-    window.addEventListener("scroll", isSticky);
-    return () => {
-      window.removeEventListener("scroll", isSticky);
-    };
-  });
-
-  const isSticky = () => {
-    const header = document.querySelector('.header-section');
-    const scrollTop = window.scrollY;
-    scrollTop >= 120 ? header.classList.add('is-sticky') :
-    header.classList.remove('is-sticky');
-  };
-=======
-
->>>>>>> origin/main
 
   const closeMenu = () => {
     if (window.innerWidth <= 991) {
@@ -58,14 +34,6 @@ const Header = () => {
     <header className="header-section">
       <Container>
         <Navbar expand="lg" className="p-0">
-<<<<<<< HEAD
-          {/* Logo Section  */}
-          <Navbar.Brand>
-            <NavLink to="/"> Weekendmonks</NavLink>
-          </Navbar.Brand>
-          {/* End Logo Section  */}
-
-=======
           {/* Logo Section */}
           <Navbar.Brand>
             <NavLink to="/">
@@ -75,7 +43,6 @@ const Header = () => {
           {/* End Logo Section */}
 
           {/* Navbar Links */}
->>>>>>> origin/main
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-lg`}
             aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
@@ -89,15 +56,6 @@ const Header = () => {
               </span>
             </Offcanvas.Header>
             <Offcanvas.Body>
-<<<<<<< HEAD
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <NavLink className="nav-link" to="/" onClick={closeMenu}>Home</NavLink>
-                <NavLink className="nav-link" to="/about-us" onClick={closeMenu}>ABOUT US</NavLink>
-                <NavLink className="nav-link" to="/tours" onClick={closeMenu}>TOURS</NavLink>
-                <NavDropdown title="DESTINATION" id={`offcanvasNavbarDropdown-expand-lg`}>
-                  <NavLink className="nav-link text-dark" to="/destinations" onClick={closeMenu}>SPAIN TOURS</NavLink>
-                </NavDropdown>
-=======
               <Nav className="justify-content-center flex-grow-1 pe-3">
                 <NavLink className="nav-link" to="/" onClick={closeMenu}>Home</NavLink>
                 <NavLink className="nav-link" to="/about-us" onClick={closeMenu}>ABOUT US</NavLink>
@@ -106,32 +64,18 @@ const Header = () => {
                   </NavLink>
                 <NavLink className="nav-link" to="/tours" onClick={closeMenu}>TOURS</NavLink>
                 <NavLink className="nav-link" to="/top-place" onClick={closeMenu}>TOP PLACE</NavLink>
->>>>>>> origin/main
                 <NavLink className="nav-link" to="/gallery" onClick={closeMenu}>GALLERY</NavLink>
                 <NavLink className="nav-link" to="/contact-us" onClick={closeMenu}>CONTACT</NavLink>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
-<<<<<<< HEAD
-
-=======
           {/* End Navbar Links */}
 
           {/* Book Now Button */}
->>>>>>> origin/main
           <div className="ms-md-4 ms-2">
             <NavLink className="primaryBtn d-none d-sm-inline-block" onClick={toggleModal}>
               Book Now
             </NavLink>
-<<<<<<< HEAD
-            <li className="d-inline-block d-lg-none ms-3 toggle_btn">
-              <i className={open ? "bi bi-x-lg" : "bi bi-list"} onClick={toggleMenu}></i>
-            </li>
-          </div>
-        </Navbar>
-      </Container>
-      
-=======
           </div>
 
           {/* Mobile Menu Toggle */}
@@ -141,7 +85,6 @@ const Header = () => {
         </Navbar>
       </Container>
 
->>>>>>> origin/main
       {/* Modal Đặt Tour */}
       <BookingModal isOpen={isModalOpen} toggle={toggleModal} />
     </header>
