f66c9fba85948747861aca6dd44ef2cd8252404b
VuongLe <lqv1993@gmail.com>
2024-09-19 13:40:33 +0700

    commit

--- techwiz/src/App.js
+++ techwiz/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Header from "./components/Common/Header/Header";
+import Footer from "./components/Common/Footer/Footer";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Tours from "./pages/Tours/Tours";
+import TourDetails from "./pages/Tours/TourDetails";
+import Booking from "./pages/Booking/Booking";
+import Destinations from "./pages/Destinations/Destinations";
+import PhotoGallary from "./pages/PhotoGallery/PhotoGallery";
+import PhotoGallery from "./pages/PhotoGallery/PhotoGallery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about-us" element={<About />} />
+        <Route path="contact-us" element={<Contact />} />
+        <Route path="tours" element={<Tours />} />
+        <Route path="tour-details" element={<TourDetails />} />
+        <Route path="booking" element={<Booking />} />
+        <Route path="destinations" element={<Destinations />} />
+        <Route path="gallery" element={<PhotoGallery />} />
+
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- testt/src/components/Common/Header/Header.jsx
+++ testt/src/components/Common/Header/Header.jsx
@@ -1,115 +1,96 @@
 import React, { useEffect, useState } from "react";
 import {
   Container,
-  Row,
   Navbar,
   Offcanvas,
   Nav,
   NavDropdown,
 } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import BookingModal from "../../Booking Modal/BookingModal";
 import "../Header/header.css";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false); // Trạng thái cho modal
 
   const toggleMenu = () => {
     setOpen(!open);
   };
 
-  useEffect(()=>{
-    window.addEventListener("scroll", isSticky);
-    return ()=>{
-      window.removeEventListener("scroll", isSticky)
-    }
-  })
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
 
-  // sticky Header 
-  const isSticky=(e)=>{
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  });
+
+  const isSticky = () => {
     const header = document.querySelector('.header-section');
     const scrollTop = window.scrollY;
     scrollTop >= 120 ? header.classList.add('is-sticky') :
-    header.classList.remove('is-sticky')
-  }
+    header.classList.remove('is-sticky');
+  };
 
-  const closeMenu =()=>{
-    if(window.innerWidth <= 991){
-      setOpen(false)
+  const closeMenu = () => {
+    if (window.innerWidth <= 991) {
+      setOpen(false);
     }
-  }
- 
+  };
 
   return (
-    
     <header className="header-section">
       <Container>
-       
-          <Navbar expand="lg" className="p-0">
-            {/* Logo Section  */}
-            <Navbar.Brand>
-              <NavLink to="/"> Weekendmonks</NavLink>
-            </Navbar.Brand>
-            {/* End Logo Section  */}
+        <Navbar expand="lg" className="p-0">
+          {/* Logo Section  */}
+          <Navbar.Brand>
+            <NavLink to="/"> Weekendmonks</NavLink>
+          </Navbar.Brand>
+          {/* End Logo Section  */}
 
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-lg`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-              placement="start"
-              show={open}
-            >
-              {/*mobile Logo Section  */}
-              <Offcanvas.Header>
-                <h1 className="logo">Weekendmonks</h1>
-                <span className="navbar-toggler ms-auto"  onClick={toggleMenu}>
-                  <i className="bi bi-x-lg"></i>
-                </span>
-              </Offcanvas.Header>
-              {/*end mobile Logo Section  */}
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-lg`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+            placement="start"
+            show={open}
+          >
+            <Offcanvas.Header>
+              <h1 className="logo">Weekendmonks</h1>
+              <span className="navbar-toggler ms-auto" onClick={toggleMenu}>
+                <i className="bi bi-x-lg"></i>
+              </span>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavLink className="nav-link" to="/" onClick={closeMenu}>Home</NavLink>
+                <NavLink className="nav-link" to="/about-us" onClick={closeMenu}>ABOUT US</NavLink>
+                <NavLink className="nav-link" to="/tours" onClick={closeMenu}>TOURS</NavLink>
+                <NavDropdown title="DESTINATION" id={`offcanvasNavbarDropdown-expand-lg`}>
+                  <NavLink className="nav-link text-dark" to="/destinations" onClick={closeMenu}>SPAIN TOURS</NavLink>
+                </NavDropdown>
+                <NavLink className="nav-link" to="/gallery" onClick={closeMenu}>GALLERY</NavLink>
+                <NavLink className="nav-link" to="/contact-us" onClick={closeMenu}>CONTACT</NavLink>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
 
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <NavLink className="nav-link" to="/" onClick={closeMenu}>
-                    Home
-                  </NavLink>
-                  <NavLink className="nav-link" to="/about-us" onClick={closeMenu}>
-                    ABOUT US
-                  </NavLink>
-                  <NavLink className="nav-link" to="/tours" onClick={closeMenu}>
-                    TOURS
-                  </NavLink>
-
-                  <NavDropdown
-                    title="DESTINATION"
-                    id={`offcanvasNavbarDropdown-expand-lg`}
-                  >
-                   
-                      
-                    <NavLink className="nav-link text-dark" to="/destinations" onClick={closeMenu}>
-                    SPAIN TOURS
-                  </NavLink>
-                  
-                   
-                  </NavDropdown>
-                  <NavLink className="nav-link" to="/gallery" onClick={closeMenu}>
-                    GALLERY
-                  </NavLink>
-                  <NavLink className="nav-link" to="/contact-us" onClick={closeMenu}>
-                    CONTACT
-                  </NavLink>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-            <div className="ms-md-4 ms-2">
-              <NavLink className="primaryBtn d-none d-sm-inline-block">
-                Book Now
-              </NavLink>
-              <li className="d-inline-block d-lg-none ms-3 toggle_btn">
-                <i className={open ? "bi bi-x-lg" : "bi bi-list"}  onClick={toggleMenu}></i>
-              </li>
-            </div>
-          </Navbar>
-    
+          <div className="ms-md-4 ms-2">
+            <NavLink className="primaryBtn d-none d-sm-inline-block" onClick={toggleModal}>
+              Book Now
+            </NavLink>
+            <li className="d-inline-block d-lg-none ms-3 toggle_btn">
+              <i className={open ? "bi bi-x-lg" : "bi bi-list"} onClick={toggleMenu}></i>
+            </li>
+          </div>
+        </Navbar>
       </Container>
+      
+      {/* Modal Đặt Tour */}
+      <BookingModal isOpen={isModalOpen} toggle={toggleModal} />
     </header>
   );
 };
